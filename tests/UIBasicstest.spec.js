c5e9dbfc6c498b87be04462057b0baf28370cd89
VaibhaV Arde <vaibhav.arde@gmail.com>
2024-11-12 20:41:09 +0530

    playwright playwright.config.js and test execution options

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -7,9 +7,9 @@ const {test} = require('@playwright/test')
 test('Browser Context Playwright test', async ({browser}) => {
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    await page.goto("https://google.com")
 });
 
-test('Page Playwright test', async ({page}) => {
+test.only('Page Playwright test', async ({page}) => {
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
 });

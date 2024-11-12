9275dbd65e3802974435599cdf333981eb4f9cd2
VaibhaV Arde <vaibhav.arde@gmail.com>
2024-11-12 19:57:42 +0530

    global fixtures

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -4,6 +4,12 @@ const {test} = require('@playwright/test')
 
 // });
 
-test('First Playwright test', async () => {
+test('Browser Context Playwright test', async ({browser}) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+});
 
+test('Page Playwright test', async ({page}) => {
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
 });

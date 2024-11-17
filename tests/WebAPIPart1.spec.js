2e33c4e37ed8ecad058e2a027d715fb183d940db
VaibhaV Arde <vaibhav.arde@gmail.com>
2024-11-17 21:15:48 +0530

    Minor changes

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -10,36 +10,29 @@ test.beforeAll( async()=>
    const apiContext = await request.newContext();
    const apiUtils = new APiUtils(apiContext,loginPayLoad);
    response =  await apiUtils.createOrder(orderPayLoad);
-
 })
 
 
 //create order is success
-test('@API Place the order', async ({page})=>
-{ 
+test('@API Place the order', async ({page})=>{
     page.addInitScript(value => {
-
         window.localStorage.setItem('token',value);
     }, response.token );
-await page.goto("https://rahulshettyacademy.com/client");
- await page.locator("button[routerlink*='myorders']").click();
- await page.locator("tbody").waitFor();
-const rows = await page.locator("tbody tr");
-
-
-for(let i =0; i<await rows.count(); ++i)
-{
-   const rowOrderId =await rows.nth(i).locator("th").textContent();
-   if (response.orderId.includes(rowOrderId))
-   {
-       await rows.nth(i).locator("button").first().click();
-       break;
-   }
-}
-const orderIdDetails =await page.locator(".col-text").textContent();
-//await page.pause();
-expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
-
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.locator("tbody").waitFor();
+    const rows = await page.locator("tbody tr");
+    
+    for(let i =0; i<await rows.count(); ++i){
+        const rowOrderId =await rows.nth(i).locator("th").textContent();
+        if (response.orderId.includes(rowOrderId)){
+            await rows.nth(i).locator("button").first().click();
+            break;
+        }
+    }
+    const orderIdDetails =await page.locator(".col-text").textContent();
+    //await page.pause();
+    expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
 });
 
 //Verify if order created is showing in history page

d8f8e6ae6f85aa065a7b2317bb166c587d211d3c
VaibhaV Arde <vaibhav.arde@gmail.com>
2024-11-18 22:43:52 +0530

    Minor changes

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -22,7 +22,7 @@ test('@API Place the order', async ({page})=>{
     await page.locator("button[routerlink*='myorders']").click();
     await page.locator("tbody").waitFor();
     const rows = await page.locator("tbody tr");
-    
+
     for(let i =0; i<await rows.count(); ++i){
         const rowOrderId =await rows.nth(i).locator("th").textContent();
         if (response.orderId.includes(rowOrderId)){
@@ -34,6 +34,3 @@ test('@API Place the order', async ({page})=>{
     //await page.pause();
     expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
 });
-
-//Verify if order created is showing in history page
-// Precondition - create order -

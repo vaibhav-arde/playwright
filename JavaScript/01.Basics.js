5c5b393f73bf71d04c390c96b7f175e6b8f800e0
VaibhaV Arde <vaibhav.arde@gmail.com>
2024-11-26 08:47:07 +0530

    JS fundamentals

---
+++ JavaScript/01.Basics.js
@@ -0,0 +1,45 @@
+// Javascript Hellow World program
+console.log("Hello World!!")
+
+// comments
+
+/*
+This 
+is Multi
+Line
+Comment
+*/
+
+console.log("**************************")
+// Declaring variables in javascript
+// Variables
+var name="Vaibhav"
+
+// es6 -- let const
+let a = 4
+console.log(a)
+
+console.log("**************************")
+// Understanding the datatypes in JS
+console.log(typeof(a))
+
+let b = 234.5
+console.log(typeof(b))
+
+let c = "Vaibhav Arde"
+console.log(typeof(c))
+
+let required = true
+console.log(typeof(required))
+// null and undefined
+
+let d
+console.log(typeof(d))
+
+let e = null
+console.log(typeof(e))
+
+c = a+b
+console.log("c", c)
+
+console.log(!required)

f42dfe8c6a5195d9d51f985b185e8cb65713b637
VaibhaV Arde <vaibhav.arde@gmail.com>
2024-11-26 18:46:39 +0530

    Classes in JS

---
+++ JavaScript/07.ClassesInJS.js
@@ -0,0 +1,21 @@
+// Class
+
+class Person{
+    constructor(firstName, lastName){
+        this.firstName = firstName
+        this.lastName =  lastName
+    }
+    age =25
+    get location(){
+        return "Dubai"
+    }
+    fullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+let person = new Person("VaibhaV", "Arde")
+console.log(person.age)
+console.log(person.location)
+console.log(person.fullName())
+

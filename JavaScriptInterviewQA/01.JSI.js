5606c25c853960cd907fe2d9e38b303b09029faa
VaibhaV Arde <vaibhav.arde@gmail.com>
2024-12-11 17:33:44 +0530

    Added new interview questions

--- JavaScriptInterviewQA/01.JSI.js
+++ JavaScriptInterviewQA/01.JSI.js
@@ -5,6 +5,8 @@
 // }
 // *******************************
 
+const { reverse } = require("dns")
+
 // // Q2 - What is function expression ? annonymous function ?
 // const square = function(num){
 //         return num **2
@@ -12,7 +14,7 @@
 // *******************************
 
 // // Q3 - What are first class functions?
-// // Functions can be treated as a variable, can be passed to another function, can be used manipulated and returned from function jsut the way variable can do.
+// // Functions can be treated as a variable, can be passed to another function, can be used, manipulated and returned from function just the way variable can do.
 // function square(num){
 //     return num * num;
 // }
@@ -90,7 +92,7 @@
 // *******************************
 
 // // Q12 - Callback function 
-// // Callback function is a function passed into another function as an argumanet, 
+// // Callback function is a function passed into another function as an argument, 
 // // which is then invoked inside the outer function to complete some kind of routine or action
 // function greeting(name){
 //     alert("Hello "+ name);
@@ -158,3 +160,183 @@
 
 // var myFunc = makeFunc()
 // myFunc()
+
+// *******************************
+// 2. Given an integer, find the nearest palindrome to that number.
+// //   Eg1. I/P : 115 O/P : 111
+// //   Eg2. I/P : 117 O/P : 121
+// function isPalindrome(n){
+//     numStr = n.toString()
+//     reverseStr = numStr.split("").reverse().join("")
+//     return numStr === reverseStr
+// }
+
+// function nearestPalindrome(num){
+//     if (num <=0 )return "Number should be greater than 0";
+
+//     if(isPalindrome(num)){
+//         console.log(`Given number is a palindrome ${num}`)
+//         return num
+//     }
+//     lower = num - 1;
+//     higher = num + 1;
+//     while(true){
+//         if(isPalindrome(lower)){
+//             console.log(`Nearest palindrome is ${lower}`)
+//             return lower
+//         }
+//         if(isPalindrome(higher)){
+//             console.log(`Nearest palindrome is ${lower}`)
+//             return lower
+//         }
+//         lower--;
+//         higher++;
+//     }
+// }
+
+
+// nearestPalindrome(121)
+
+// // *******************************
+// // Count the occurrence of each character in a string using a simple loop, forEach()
+// const str = 'hello world';
+// const charCount = {};
+
+// str.split('').forEach(char => {
+//   charCount[char] = (charCount[char] || 0) + 1;
+// });
+
+// console.log(charCount);
+// // Output: { h: 1, e: 1, l: 3, o: 2, ' ': 1, w: 1, r: 1, d: 1 }
+
+// // *******************************
+// create an object from two arrays using forEach()
+// const keys = ['name', 'age', 'city'];
+// const values = ['Vaibhav', 30, 'Pune'];
+
+// const obj = {};
+// keys.forEach((key, index) => {
+//   obj[key] = values[index];
+// });
+
+// console.log(obj);
+// // Output: { name: 'Vaibhav', age: 30, city: 'Pune' }
+
+
+// // // *******************************
+// // // difference between **`for...in`** and **`for...of`** with examples.
+
+// // ---
+
+// // ### **1️⃣ `for...in`**
+// // - **Use case**: Iterates over **keys (properties)** of an object or **indexes** of an array.
+// // - **Works with**: Objects and Arrays.
+// // - **Returns**: Property names (keys) or indexes (for arrays).
+
+// // #### **Example with Object**
+// // ```javascript
+// const obj = { name: 'Vaibhav', age: 30, city: 'Pune' };
+
+// for (const key in obj) {
+//   console.log(`${key}: ${obj[key]}`);
+// }
+// // // Output:
+// // // name: Vaibhav
+// // // age: 30
+// // // city: Pune
+// // ```
+
+// // #### **Example with Array**
+// // ```javascript
+// const arr = ['a', 'b', 'c'];
+
+// for (const index in arr) {
+//   console.log(`Index: ${index}, Value: ${arr[index]}`);
+// }
+// // // Output:
+// // // Index: 0, Value: a
+// // // Index: 1, Value: b
+// // // Index: 2, Value: c
+// // ```
+
+// // ---
+
+// // ### **2️⃣ `for...of`**
+// // - **Use case**: Iterates over **values** of an iterable (like arrays, strings, Maps, Sets, etc.).
+// // - **Works with**: Arrays, Strings, Maps, Sets, etc.
+// // - **Returns**: The **value** directly (not the index or key).
+
+// // #### **Example with Array**
+// // ```javascript
+// const arr = ['a', 'b', 'c'];
+
+// for (const value of arr) {
+//   console.log(value);
+// }
+// // // Output:
+// // // a
+// // // b
+// // // c
+// // ```
+
+// // #### **Example with String**
+// // ```javascript
+// const str = 'hello';
+
+// for (const char of str) {
+//   console.log(char);
+// }
+// // // Output:
+// // // h
+// // // e
+// // // l
+// // // l
+// // // o
+// // ```
+
+// // ---
+
+// // ### 🔥 **Key Differences**
+// // | **Feature**      | **`for...in`**                  | **`for...of`**                 |
+// // |-----------------|---------------------------------|---------------------------------|
+// // | **Iterates**     | Keys (indexes)                 | Values                         |
+// // | **Works on**     | Objects, Arrays, Strings       | Arrays, Strings, Maps, Sets    |
+// // | **Return**       | Property names or indexes     | Values from iterables           |
+// // | **Use case**     | Loop over object properties    | Loop over array or string values|
+// // | **Example**      | `for (let key in obj) {}`     | `for (let val of arr) {}`      |
+
+// // ---
+
+// // ### **When to use what?**
+// // - Use **`for...in`** to iterate over **keys of objects**.
+// // - Use **`for...of`** to iterate over **values of arrays, strings, or iterables**.
+
+// // // *******************************
+// // To get the character with the maximum consecutive occurrence and its count from a string
+// const str = 'aaabbccccddeeeaa';
+
+// let maxChar = '';
+// let maxCount = 0;
+
+// let currentChar = '';
+// let currentCount = 0;
+
+// for (let i = 0; i < str.length; i++) {
+//   if (str[i] === currentChar) {
+//     currentCount++;
+//   } else {
+//     currentChar = str[i];
+//     currentCount = 1;
+//   }
+  
+//   if (currentCount > maxCount) {
+//     maxCount = currentCount;
+//     maxChar = currentChar;
+//   }
+// }
+
+// console.log(`Character: '${maxChar}', Count: ${maxCount}`);
+// // Output: Character: 'c', Count: 4
+
+
+// // // *******************************
